1c6ba46e14514062a8a60899830ad945b75be0c6
Camilo Verdugo <camilo.verdugo@gmail.com>
2017-12-19 17:20:53 -0300

    package builder dir

--- install.sh
+++ install.sh
@@ -1,9 +1,12 @@
-# pip3 uninstall -y django_mailtor &&
+pip3 uninstall -y django_mailtor &&
+
+cp readme.md package/django_mailtor
+cp requirements.txt package/django_mailtor
 
 # Packing new version
 cd package/django_mailtor/ &&
 python3 setup.py sdist &&
 
 #Install new version
-pip3 install --user dist/django_mailtor-0.0.1.tar.gz &&
+pip3 install --user dist/django_mailtor-0.0.2.tar.gz &&
 cd ../../
