703a7da4160def0f003b13cf01a9785b9d5b893c
Camilo Verdugo <camilo.verdugo@gmail.com>
2018-02-06 14:30:19 -0300

    upgrade url

--- django_mailtor/urls.py
+++ django_mailtor/urls.py
@@ -13,14 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url, include
+from django.conf.urls import include
+from django.urls import path
 from django.contrib import admin
 from django.conf import settings
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^mailtor/', include('mailtor.urls')),
+    path('admin/', admin.site.urls),
+    path('mailtor/', include('mailtor.urls')),
 ]
 
 if settings.DEBUG:
--- mailtor/urls.py
+++ mailtor/urls.py
@@ -1,10 +1,10 @@
-from django.conf.urls import url
+from django.urls import path
 
 from . import views
 
 urlpatterns = [
-    url(r'^create_template', views.create_template, name='create_template'),
-    url(r'^create_entity', views.create_entity, name='create_entity'),
-    url(r'^tracking_open/(?P<mail_id>\w+)$', views.tracking_open, name='tracking_open'),
-    url(r'^home', views.index, name="mailtor")
+    path('create_template', views.create_template, name='create_template'),
+    path('create_entity', views.create_entity, name='create_entity'),
+    path('tracking_open/<str:mail_id>', views.tracking_open, name='tracking_open'),
+    path('home', views.index, name="mailtor")
 ]
